da074bcd5f7a1c128c8bd666aaa28b253cdbb19e
sirikub <kubyshkinsergei21@gmail.com>
2023-10-06 19:01:58 +0300

    fix (renderField)

--- app.js
+++ app.js
@@ -1,68 +1,21 @@
-/**
- * x, y размер квадрата в игровом поле
- * @param size размер клетки
- * @param y - Number of times
- */
-function renderFindPlayer(size){
-    //const canvas = document.getElementById("playingField");
-
-    // canvas.style.width = "200px";
-    // canvas.style.height = "200px";
-    // let x = 0 // размер клетки
-    // ctx.lineWidth = 1;
-    // // for (let i=0; i<=20; i++) {     
-    // //         x = x + 20
-    // //         ctx.moveTo(x, 0)
-    // //         ctx.lineTo(x, 190);   
-    // // }
-
-    // // for (let a = 0; a <= 200; a += 20.5) {
-    // //     ctx.moveTo(a, 0);
-    // //     ctx.lineTo(a, 190);
-    // //   }
-    // //   for (let b = 0.5; b < 290; b += 31.55) {
-    // //     ctx.moveTo(0, b);
-    // //     ctx.lineTo(290, b);
-    // //   }
-    //   //ctx.strokeStyle = "#888";
-    //   ctx.stroke();
-    // }
-
-
-    var canvas = document.getElementById('playingField')
-    var ctx = canvas.getContext('2d') 
-    ctx.rect(0, 0, 200, 200)
-    
-    var size = 160
-    canvas.style.width = size + "px"
-    canvas.style.height = size + "px"
-    
-    // Set actual size in memory (scaled to account for extra pixel density).
-    var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
-    canvas.width = size * scale;
-    canvas.height = size * scale;
-    
-    let x = 0 // размер клетки
-
-    let y = 0 
-    let width = 0
-    
-    for (let i=0;i<100;i++){         
-        ctx.rect(x, y, 20, 20)
-        
-        ctx.stroke();
-        x = x + 20
-        width = width + 20        
-        if (canvas.width == width){             
-            x = 0
-            width = 0
-            y = y + 20
-        }  
-        console.log(width, x, y, canvas.width)    
- 
-}
+/**@param size элемент в котором рисуем игровое поле */
+function renderField(className){
+    function createDiv(num){       
+        const div = document.createElement('span');    
+        div.className = `field child${num}`  
+        // div.style.width  = '20px'
+        // div.style.height = '20px'       
+        // div.style.float = 'left'
+        // div.style.outline = '1px solid #000000'        
+        return div
+      }
+      
+      let parent = document.getElementById(`${className}`);
+      for (var i = 0; i < 100; i++) {
+        parent.appendChild(createDiv(i));
+      }
 }
 
-renderFindPlayer(20)
+renderField('playingField')
 
 

b4d9b1f8346bb2cdd3aa58733342a8ad907d6f09
sirikub <kubyshkinsergei21@gmail.com>
2023-10-06 23:43:05 +0300

    добавлено второе поле

--- app.js
+++ app.js
@@ -2,27 +2,32 @@
 function renderField(className){
     function createDiv(num){       
         const div = document.createElement('span');    
-        div.className = `field child${num}`          
+        div.className = `fieldChild numField${num}`          
         return div
       }
       
-      let parent = document.getElementById(`${className}`);
+      let parent = document.querySelector(`${className}`);
       for (var i = 0; i < 100; i++) {
         parent.appendChild(createDiv(i));
       }
 }
 
-renderField('playingField')
-
+renderField('.playingField.computerField')
+renderField('.playingField.myField')
 
 const arrayFieldUp = ['A','Б','В','Г','Е','Д','Ж','З','И','К'] 
 const arrayFieldLeft = [1,2,3,4,5,6,7,8,9,10] 
 
-document.getElementById('playingFieldUp').innerHTML = renderText(arrayFieldUp, 'playingFieldUp')
-document.getElementById('playingFieldLeft').innerHTML = renderText(arrayFieldLeft, 'playingFieldLeft')
+// для поля компьютера (нумерация строк и столбцов)
+document.querySelector('.playingFieldUp.computer').innerHTML = renderText(arrayFieldUp, 'playingFieldUpChild')
+document.querySelector('.playingFieldLeft.computer').innerHTML = renderText(arrayFieldLeft, 'playingFieldLeftChild')
+
+// для поля игрока (нумерация строк и столбцов)
+document.querySelector('.playingFieldUp.myField').innerHTML = renderText(arrayFieldUp, 'playingFieldUpChild')
+document.querySelector('.playingFieldLeft.myField').innerHTML = renderText(arrayFieldLeft, 'playingFieldLeftChild')
 
 function renderText(array, className){    
-    return array.map((elem, index) => {
-      return `<span class="${className} child${index}">${elem}</span>`}).join('')
+    return array.map((elem) => {
+      return `<span class="${className}">${elem}</span>`}).join('')
 }
 
